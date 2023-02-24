d60b9bd362b6b00aee27007e14b45e0b94c0d837
Michelle Wren <rubblecoder@gmail.com>
2023-02-24 01:49:32 -0500

    Update Chatroom.js
    Start of the chatroom code.  Able to save state of user selection of mic on or off.

--- src/pages/Chatroom.js
+++ src/pages/Chatroom.js
@@ -1,28 +1,46 @@
+import {useState} from 'react';
 import '../assets/styles/chatroom.scss';
 import micIcon from '../assets/images/microphone.png';
+import micIconGreen from '../assets/images/microphone-green.png';
+import micIconRed from '../assets/images/microphone-red.png';
 import rightArrow from '../assets/images/right-arrow.png';
 
 function Chatroom() {
+    const [image, setImage] = useState(micIcon);
+    const [micStatus, setMicStatus] = useState(false);
+  
+    function toggleMic() {
+        if (micStatus) {
+            //stopMic();
+            setMicStatus(false);
+            setImage(micIconRed);
+        } else {
+            //startMic();
+            setMicStatus(true);
+            setImage(micIconGreen);
+        }
+    }
+
   return (
     <div className="chatroom">
-        <div id="main-container">
-            <div class="source-pane">
-                <div class="pane-header">English</div>
-                <div id="en-msg-container" class="translation-content"></div>
+        <div id="main-chatroom-container">
+            <div className="source-pane">
+                <div className="pane-header">English</div>
+                <div id="en-msg-container" className="translation-content">This is a test</div>
             </div>
-            <div class="config-pane">
-                <div class="pane-header">Translate</div>
-                <div class="mic-icon-container">
-                    <img id="mic-icon" class="mic-icon" src={micIcon} onclick="toggleMic()" border="0" />
+            <div className="config-pane">
+                <div className="pane-header opp-text">Translate</div>
+                <div className="mic-icon-container">
+                    <img alt="Toggle Mic Button" id="mic-icon" className="mic-icon" src={image} border="0" onClick={toggleMic} />
                 </div>
-                <div class="text-input-container">
+                <div className="text-input-container" valign="bottom">
                     <input type="text" id="text-input-msg" placeholder="Write text to speech here..." />
-                    <img id="go-arrow" src={rightArrow} onclick="sendMessage()" />
+                    <img alt="Send Text Button" id="go-arrow" src={rightArrow}  />
                 </div>
             </div>
-            <div class="translation-pane">
-                <div class="pane-header">Spanish</div>
-                <div id="sp-msg-container" class="translation-content"></div>
+            <div className="translation-pane">
+                <div className="pane-header">Spanish</div>
+                <div id="sp-msg-container" className="translation-content">This is a test <br/>This is a test</div>
             </div>
         </div>
     </div>
